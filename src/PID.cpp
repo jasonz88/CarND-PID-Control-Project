f3f2692bc7382aeea6b96a005aed444383693c78
dyz <jason.dyzhang88@gmail.com>
2017-09-04 23:03:05 -0700

    indent

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,43 +12,43 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-	this->Kp = Kp;
-	this->Ki = Ki;
-	this->Kd = Kd;
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
 
-	p_error = 0;
-	i_error = 0;
-	d_error = 0;
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
 }
 
 void PID::Update(double KpCoeff, double KiCoeff, double KdCoeff, double v) {
-	Kp += 1e-5 * Kp * KpCoeff * v;
-	Ki += 1e-4 * Ki * KiCoeff * v;
-	Kd += 1e-4 * Kd * KdCoeff * v;
-	if (0.15 < Kp) {
-		Kp = 0.15;
-	}
-	if (0.002 < Ki) {
-		Ki = 0.002;
-	}
-	if (4.0 < Kd) {
-		Kd = 4.0;
-	}
+    Kp += 1e-5 * Kp * KpCoeff * v;
+    Ki += 1e-4 * Ki * KiCoeff * v;
+    Kd += 1e-4 * Kd * KdCoeff * v;
+    if (0.15 < Kp) {
+        Kp = 0.15;
+    }
+    if (0.002 < Ki) {
+        Ki = 0.002;
+    }
+    if (4.0 < Kd) {
+        Kd = 4.0;
+    }
 }
 
 double PID::UpdateError(double cte) {
-	double pre_cte = p_error;
+    double pre_cte = p_error;
 
-	p_error  = cte;
-	i_error += cte;
-	d_error  = cte - pre_cte;
-	return fabs(cte-pre_cte);
+    p_error  = cte;
+    i_error += cte;
+    d_error  = cte - pre_cte;
+    return fabs(cte-pre_cte);
 }
 
 double PID::TotalError() {
-	return -Kp*p_error - Ki*i_error - Kd*d_error;
+    return -Kp*p_error - Ki*i_error - Kd*d_error;
 }
 
 double PID::OutputThrottle(double max_thro) {
-	return max_thro - Kp*p_error - Ki*i_error - Kd*d_error;
+    return max_thro - Kp*p_error - Ki*i_error - Kd*d_error;
 }
